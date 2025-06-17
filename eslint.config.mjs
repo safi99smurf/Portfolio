ecc8c9cb81ff6eb0d4da8d25aab698eb401f2bbd
alisafi99 <108078233+alisafisyed@users.noreply.github.com>
2025-06-17 21:42:00 +0500

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Use Next.js core recommendations
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Custom override to disable the apostrophe rule
+  {
+    files: ["**/*.tsx", "**/*.jsx"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
